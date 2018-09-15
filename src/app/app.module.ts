d8ac44b5b244e9f5027dba1a7753f76296df0506
Brad-- <brad.smith.1324@gmail.com>
2018-09-15 16:19:34 -0600

    Update insecure dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './app.material.module';
 
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule
   ],
   providers: [],
